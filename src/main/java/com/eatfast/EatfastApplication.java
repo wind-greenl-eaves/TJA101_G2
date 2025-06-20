bc682352b14b19898352ce5f57a377eb87400d47
H.Y.Fn <young19960127@gmail.com>
2025-06-20 17:43:28 +0800

    first initial commit

---
+++ src/main/java/com/eatfast/EatfastApplication.java
@@ -0,0 +1,13 @@
+package com.eatfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatfastApplication.class, args);
+	}
+
+}
